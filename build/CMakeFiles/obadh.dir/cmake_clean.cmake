7880d73772521cf0c73cca3260637f3ed8c56341
nsssayom <nsssayom@gmail.com>
2024-02-16 16:59:12 +0600

    simple words and vowel-modifiers working

---
+++ build/CMakeFiles/obadh.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obadh.dir/src/Transliterator.cpp.o"
+  "CMakeFiles/obadh.dir/src/Transliterator.cpp.o.d"
+  "CMakeFiles/obadh.dir/src/main.cpp.o"
+  "CMakeFiles/obadh.dir/src/main.cpp.o.d"
+  "obadh"
+  "obadh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obadh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
